68dfa86fa960de2c1d69956415d79ca2296cb181
Lauren Judge <l.m.judge@hotmail.com>
2020-10-02 16:43:16 +1000

    added basic user data model and method

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
+const http = require('http');
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/APIAuth>');
+mongoose.connect('mongodb://localhost:27017/node-angular',  {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => {
+    console.log('Connected to Database');
+  })
+  .catch((err) => {
+    console.log(err)
+  });
 
 const app = express();
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(bodyParser.json());
+
 
 app.use('/users', require('./routes/users'))
 
-const port = process.env.PORT || 3000;
-app.listen(port);
-console.log(`Server listening at ${port}`);
+const port = 3000;
+const hostname = '127.0.0.1';
+
+
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
--- controllers/users.js
+++ controllers/users.js
@@ -1,13 +1,23 @@
+const User = require('../models/users');
+
 module.exports = {
   signUp: async(req, res, next) => {
-    console.log('UsersController.Signup() called')
+    const { email, password } = req.body;
+
+    const foundUser = await User.findOne({ email });
+    if(foundUser) {
+      return res.status(403).json({ error: 'Email is already in use' });
+    }
+
+    const newUser = new User({ email, password })
+    await newUser.save();
+    res.json({ user: 'created' })
+    console.log(newUser);
   },
 
   signIn: async(req, res, next) => {
-    console.log('UsersController.Signin() called')
   },
 
   getPosts: async(req, res, next) => {
-    console.log('UsersController.getPosts() called')
   }
 }
