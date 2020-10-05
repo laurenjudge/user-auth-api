61353f4b8d0eaac5e44bd356d536f55dc5c32fb7
Lauren Judge <l.m.judge@hotmail.com>
2020-10-05 11:14:17 +1100

    password salt & hashing added

--- models/users.js
+++ models/users.js
@@ -1,8 +1,20 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true, lowercase: true },
   password: { type: String, required: true }
 });
 
-module.exports = mongoose.model('User', userSchema);;
+userSchema.pre('save', async function(next) {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(this.password, salt);
+    this.password = passwordHash;
+    next();
+  } catch(error) {
+    next(error);
+  }
+});
+
+module.exports = mongoose.model('User', userSchema);
